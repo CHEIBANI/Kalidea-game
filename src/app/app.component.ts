d560ec6a86dc50e5610471ec483e634c101ac329
cheibani cheibani <cheibanie91@gmail.com>
2022-01-26 20:25:32 +0100

    3A: Accueil replace href by routerlink
    time: 10min

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from "./user/models/user";
+import { UserService } from "./user/user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Kalidea-game';
+  public currentUser = this.userService.user$;
+
+  constructor(private userService: UserService) {}
 }
